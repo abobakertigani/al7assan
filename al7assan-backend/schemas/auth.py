93dd216569bae8cac83c1ca8b5a2f89b68c48935
abobakertigani <56824184+abobakertigani@users.noreply.github.com>
2025-07-27 11:12:57 +0200

    Update auth.py

--- al7assan-backend/schemas/auth.py
+++ al7assan-backend/schemas/auth.py
@@ -1,6 +1,18 @@
 # schemas/auth.py
 from pydantic import BaseModel
+from typing import Optional
 
+# للدخول
 class LoginSchema(BaseModel):
     email: str
     password: str
+
+# للتسجيل
+class SignupSchema(BaseModel):
+    name: str
+    email: str
+    password: str
+    company_name: str
+    company_email: Optional[str] = None
+    company_phone: Optional[str] = None
+    company_address: Optional[str] = None
